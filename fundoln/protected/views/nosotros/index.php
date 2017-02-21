69b0961238c6cecefd9ddf27921992f006ce2f9d
Jessy Albarran <jalbarran@thefactoryhka.com>
2017-02-21 16:03:47 -0400

    Modificacion de la seccion Nosotros

--- fundoln/protected/views/nosotros/index.php
+++ fundoln/protected/views/nosotros/index.php
@@ -7,25 +7,22 @@ $this->breadcrumbs=array(
 ?>
 <!--<h1><?php echo $this->id . '/' . $this->action->id; ?></h1>-->
 
+<section >
 <center>
   <h1 class="aeroregular verde"><?php echo Yii::t('nosotros','Nosotros'); ?></h1>
   </center>
-<hr style="width: 10%">
-<br>
-
-<section>
-    
+<hr style="width: 13%;border-top: 1px solid #777;">
     <div class="container">
        
         <div class="row">
             <div class="col-lg-12 wow fadeInDown">
                 <header>
-                    <h1 class="robotoblack">¿Quienes Somos?</h1>
+                    <h1 class="h1">¿Quienes Somos?</h1>
                     <br>
                    
-                      <div style="color:#fff;background-color: darkgreen" class="panel panel-info">           
+                      <div style="color:#fff;background-color: #299100" class="panel panel-info">           
                         <div class="panel-body">
-                        <p align="justify">Somos una empresa agropecuaria  fundada en el año 2015, con la finalidad de producir masivamente ganado bovino y vacuno, generando empleos y apoyando al país en una de sus necesidades básicas y de crecimiento. Con una mentalidad de no desperdicio, obtiene  el mayor rendimiento de los recursos disponibles y de apoyo al sector agropecuario de la economía venezolana, lo que es la base y soporte del país.</p>
+                        <arti id="letra" align="justify">Somos una empresa agropecuaria  fundada en el año 2015, con la finalidad de producir masivamente ganado bovino y vacuno, generando empleos y apoyando al país en una de sus necesidades básicas y de crecimiento. Con una mentalidad de no desperdicio, obtiene  el mayor rendimiento de los recursos disponibles y de apoyo al sector agropecuario de la economía venezolana, lo que es la base y soporte del país.</arti>
                         </div>           
                         </div>
                    
@@ -35,31 +32,32 @@ $this->breadcrumbs=array(
     </div>
 </section>
 <section id="nosotros">
-<div class="container">
-<div class="row col-lg-12">
-    <div class="col-md-4 col-md-offset-1 wow fadeInLeft">
-        <h3>Misión</h3> 
-        <br>
-        <p style="text-align: justify;">
-           Ser una empresa agropecuaria líder en el mercado Venezolano, de producción masiva  de ganado bovino y vacuno, alcanzando rendimientos agropecuarios sostenidos con una alta productividad y alto valor agregado; para abastecer los requerimiento del Mercado Nacional, mediante la comercialización de reses con los mas altos estándares de calidad.
-        </p>
-    </div>
+		<div class="inner">
+	<div class="container">
+					<div class="row col-lg-12">
+					    <div class="col-md-4 col-md-offset-1 text-center wow fadeInLeft">
+					        <h3>Misión</h3> 
+					        <br>
+					        <p style="text-align: justify;">
+					           Ser una empresa agropecuaria líder en el mercado Venezolano, de producción masiva  de ganado bovino y vacuno, alcanzando rendimientos agropecuarios sostenidos con una alta productividad y alto valor agregado; para abastecer los requerimiento del Mercado Nacional, mediante la comercialización de reses con los mas altos estándares de calidad.
+					        </p>
+					    </div>
 
-    <div class="col-md-4 col-md-offset-2 wow fadeInRight">
-        <h3 style="text-align: right;">Visión</h3> 
-        <br>
-        <p style="text-align: justify;">
-			Garantizar la producción agropecuaria de ganado bovino y vacuno, disminuyendo los costos de producción, desarrollando la capacitación técnico y profesional del capital humano, introduciendo nuevas tecnologías de punta, así como establecer una correcta estrategia de comercialización que permita satisfacer los requerimientos de nuestros clientes.        
-		</p>
-    </div>
-</div>
-
+					    <div class="col-md-4 col-md-offset-2 text-center wow fadeInRight">
+					        <h3>Visión</h3> 
+					        <br>
+					        <p style="text-align: justify;">
+								Garantizar la producción agropecuaria de ganado bovino y vacuno, disminuyendo los costos de producción, desarrollando la capacitación técnico y profesional del capital humano, introduciendo nuevas tecnologías de punta, así como establecer una correcta estrategia de comercialización que permita satisfacer los requerimientos de nuestros clientes.        
+							</p>
+					    </div>
+					</div>
 <br/>
 <br/>
-<div class="row col-md-10 col-md-offset-1">
-            <div class="panel-body">
-                <p align="justify"> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione distinctio blanditiis cum earum quos, saepe quia, dolor reprehenderit voluptates repellat voluptatem aperiam amet obcaecati cumque veniam neque itaque error, commodi! </p>
-            </div>           
-</div>
+			<div class="row col-md-10 col-md-offset-1 wow fadeInUp">
+			            <div class="panel-body">
+			                <p align="justify"> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione distinctio blanditiis cum earum quos, saepe quia, dolor reprehenderit voluptates repellat voluptatem aperiam amet obcaecati cumque veniam neque itaque error, commodi! </p>
+			            </div>           
+			</div>
+		</div>
 </div>
 </section>
